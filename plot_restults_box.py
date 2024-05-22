dc91d4c6aad096a7056e2a6f7dbeaba85ef0dd91
juanjo-cabrera <juanjo.cabremora@gmail.com>
2024-05-22 16:39:40 +0200

    GLOBAL result box plot added

--- plot_restults_box.py
+++ plot_restults_box.py
@@ -9,24 +9,17 @@ models_names = ['convnext']
 training_sequences_names = ['noDA', 'DA1', 'DA2', 'DA3', 'DA4', 'DA5', 'DA6']
 illuminations = ['cloudy', 'night', 'sunny']
 
-
+font = 15
 base_results = '/home/arvc/Juanjo/develop/Extension Orlando/'
-# for model_base_name in models_names:
-#     for dataset_name in training_sequences_names:
-#         for illumination in illuminations:
-#             results = base_results + illumination + '_' + model_base_name + '_' + dataset_name + '.csv'
-#             df = pd.read_csv(results)
-#             errors = df['Errors'].values.tolist()
-
-# Initialize an empty list to collect all data
 all_data = []
 
 # Read data from CSV files
 for model_base_name in models_names:
     for dataset_name in training_sequences_names:
+        global_data = []
         for illumination in illuminations:
             # Construct the file path
-            results =  base_results + illumination + '_' + model_base_name + '_' + dataset_name + '.csv'
+            results = base_results + illumination + '_' + model_base_name + '_' + dataset_name + '.csv'
 
             # Check if the file exists
             if os.path.exists(results):
@@ -41,31 +34,45 @@ for model_base_name in models_names:
                             'Errors': float(error.strip('[]')),
                             'Model': model_base_name,
                             'Training Sequence': dataset_name,
-                            'Illumination': illumination
+                            'Illumination': illumination.capitalize()
+                        })
+                        global_data.append({
+                            'Errors': float(error.strip('[]')),
+                            'Model': model_base_name,
+                            'Training Sequence': dataset_name,
+                            'Illumination': 'Global'
                         })
                 else:
                     print(f"'Errors' column not found in {results}")
+
+
             else:
                 print(f"File not found: {results}")
+        all_data = all_data + global_data
+
+
+illuminations = [s.capitalize() for s in illuminations]
+illuminations.append('Global')
 
 # Create a DataFrame from the unpacked data
 if all_data:
+    print(all_data)
     all_data_df = pd.DataFrame(all_data)
 
     # Print the first few rows of the dataframe to check if data is read correctly
     print(all_data_df.head())
 
     # Define custom color palette
-    custom_palette = sns.color_palette("Spectral", len(illuminations))
+    custom_palette = sns.color_palette("Spectral", len(illuminations) + 1)
 
     # Plot the data using Seaborn
     plt.figure(figsize=(15, 10))
     ax = sns.boxplot(x='Training Sequence', y='Errors', hue='Illumination', data=all_data_df, showfliers=False,
-                     palette=custom_palette)
-    plt.title('Hierarchical Localization Error for Different Backbone Models')
-    plt.xlabel('Training Dataset')
-    plt.ylabel('Hierarchical Localization Error (m)')
-    plt.legend()
+                     palette=custom_palette, hue_order=['Cloudy', 'Night', 'Sunny', 'Global'])
+    ax.set_xlabel('Backbone Models', fontsize=font)
+    ax.set_ylabel('Hierarchical Localization Error (m)', fontsize=font)
+    ax.set_title('Hierarchical Localization Error for Different Backbone Models', fontsize=font)
+    ax.legend(fontsize=font)
     # Draw horizontal grid lines
     # plt.grid(axis='y', which='minor')
     # Draw main grid lines and subgrid lines
@@ -83,10 +90,19 @@ if all_data:
         x = training_sequences_names.index(seq)
 
         print(x)
-        hue_offset = {'cloudy': -0.25, 'night': 0, 'sunny': 0.25}  # Adjust offset according to hue categories
-        ax.text(x + hue_offset[illum], - 0.08 , f'{mean_val:.2f}',
-                ha='center', va='bottom', color='black', fontsize=10)
+        factor = 0.2
+        hue_width = len(illuminations) * factor
+        hue_offset = -hue_width / 2 + illuminations.index(illum) * factor  + factor  / 2
+        # hue_offset = {'cloudy': -0.27, 'night': 0, 'sunny': 0.27, 'global': 0.40}  # Adjust offset according to hue categories
+        ax.text(x + hue_offset, - 0.09 , f'{mean_val:.2f}',
+                ha='center', va='bottom', color='black', fontsize=font*0.9)
+        ax.scatter(x + hue_offset, mean_val, color='black', s=100, zorder=3)  # zorder to draw on top of the boxes
 
+    plt.xlim(- hue_width*2/3, len(training_sequences_names) - 1 + hue_width*2/3)  # Ajusta los límites del eje x
+    plt.ylim(-0.115, 2.5)  # Ajusta los límites del eje y
+    plt.xticks(fontsize=font)
+    plt.yticks(fontsize=font)
+    plt.subplots_adjust(left=0.04, right=0.995, bottom=0.06, top=0.95)
     plt.show()
 else:
     print("No data available to plot.")
--- save_all_test_errors.py
+++ save_all_test_errors.py
@@ -61,54 +61,66 @@ def test(model, map_data, test_dataloader):
 
 
 ruta = '/home/arvc/Juanjo/develop/Extension Orlando/'
-# models_names = ['AlexNet', 'resnet_152', 'convnext', 'resnext', 'efficientnet', 'mobilenet']
-models_names = ['convnext']
-training_sequences_names = ['noDA', 'DA1', 'DA2', 'DA3', 'DA4', 'DA5', 'DA6']
-
-print('CLOUDY')
-base_results = '/home/arvc/Juanjo/develop/Extension Orlando/cloudy_convnext_'
+models_names = ['AlexNet', 'resnet_152', 'resnext', 'efficientnet', 'mobilenet']
+# models_names = ['convnext']
+training_sequences_names = ['noDA']
+illuminations = ['cloudy', 'night', 'sunny']
+# training_sequences_names = ['noDA', 'DA1', 'DA2', 'DA3', 'DA4', 'DA5', 'DA6']
+base_results = '/home/arvc/Juanjo/develop/Extension Orlando/'
+# Read data from CSV files
 for model_base_name in models_names:
+    print(model_base_name)
     for dataset_name in training_sequences_names:
-        results = base_results + dataset_name + '.csv'
-        with open(results, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Errors'])
+        print(dataset_name)
+        for illumination in illuminations:
+            print(illumination)
+            # Construct the file path
+            results = base_results + illumination + '_' + model_base_name + '_' + dataset_name + '.csv'
+            with open(results, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow(['Errors'])
 
-            model_name = ruta + model_base_name + '_' + dataset_name
-            test_model = torch.load(model_name).cuda()
-            errors = test(test_model, map_data, test_dataloader_cloudy)
-            for error in errors:
-                writer.writerow([error])
+                model_name = ruta + model_base_name + '_' + dataset_name
+                test_model = torch.load(model_name).cuda()
+                if illumination == 'cloudy':
+                    errors = test(test_model, map_data, test_dataloader_cloudy)
+                elif illumination == 'night':
+                    errors = test(test_model, map_data, test_dataloader_night)
+                elif illumination == 'sunny':
+                    errors = test(test_model, map_data, test_dataloader_sunny)
 
+                for error in errors:
+                    writer.writerow([error])
 
-print('NIGHT')
-base_results = '/home/arvc/Juanjo/develop/Extension Orlando/night_convnext_'
-for model_base_name in models_names:
-    for dataset_name in training_sequences_names:
-        results = base_results + dataset_name + '.csv'
-        with open(results, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Errors'])
-
-            model_name = ruta + model_base_name + '_' + dataset_name
-            test_model = torch.load(model_name).cuda()
-            errors = test(test_model, map_data, test_dataloader_night)
-            for error in errors:
-                writer.writerow([error])
-
-
-
-print('SUNNY')
-base_results = '/home/arvc/Juanjo/develop/Extension Orlando/sunny_convnext_'
-for model_base_name in models_names:
-    for dataset_name in training_sequences_names:
-        results = base_results + dataset_name + '.csv'
-        with open(results, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Errors'])
-
-            model_name = ruta + model_base_name + '_' + dataset_name
-            test_model = torch.load(model_name).cuda()
-            errors = test(test_model, map_data, test_dataloader_sunny)
-            for error in errors:
-                writer.writerow([error])
+#
+# print('NIGHT')
+# base_results = '/home/arvc/Juanjo/develop/Extension Orlando/night_convnext_'
+# for model_base_name in models_names:
+#     for dataset_name in training_sequences_names:
+#         results = base_results + dataset_name + '.csv'
+#         with open(results, 'w', newline='') as file:
+#             writer = csv.writer(file)
+#             writer.writerow(['Errors'])
+#
+#             model_name = ruta + model_base_name + '_' + dataset_name
+#             test_model = torch.load(model_name).cuda()
+#             errors = test(test_model, map_data, test_dataloader_night)
+#             for error in errors:
+#                 writer.writerow([error])
+#
+#
+#
+# print('SUNNY')
+# base_results = '/home/arvc/Juanjo/develop/Extension Orlando/sunny_convnext_'
+# for model_base_name in models_names:
+#     for dataset_name in training_sequences_names:
+#         results = base_results + dataset_name + '.csv'
+#         with open(results, 'w', newline='') as file:
+#             writer = csv.writer(file)
+#             writer.writerow(['Errors'])
+#
+#             model_name = ruta + model_base_name + '_' + dataset_name
+#             test_model = torch.load(model_name).cuda()
+#             errors = test(test_model, map_data, test_dataloader_sunny)
+#             for error in errors:
+#                 writer.writerow([error])
